7e8dbf35bf2ef4cdbbdc6448cfb76a737c1ee4d6
ruslan <90987143+arua2002@users.noreply.github.com>
2023-03-30 10:02:19 +0500

    Add files via upload

---
+++ Lokal/lokal/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace lokal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.действиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.пускстопToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.перезапуститьПроцессToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.progressBar1.Location = new System.Drawing.Point(12, 377);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(460, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 354);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = ":::";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Size = new System.Drawing.Size(460, 304);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Имя";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 200;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "IP";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Время ответа";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.действиеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(487, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // действиеToolStripMenuItem
+            // 
+            this.действиеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.пускстопToolStripMenuItem,
+            this.перезапуститьПроцессToolStripMenuItem});
+            this.действиеToolStripMenuItem.Name = "действиеToolStripMenuItem";
+            this.действиеToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.действиеToolStripMenuItem.Text = "Действие";
+            // 
+            // пускстопToolStripMenuItem
+            // 
+            this.пускстопToolStripMenuItem.Name = "пускстопToolStripMenuItem";
+            this.пускстопToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.пускстопToolStripMenuItem.Text = "пуск/стоп";
+            this.пускстопToolStripMenuItem.Click += new System.EventHandler(this.пускстопToolStripMenuItem_Click);
+            // 
+            // перезапуститьПроцессToolStripMenuItem
+            // 
+            this.перезапуститьПроцессToolStripMenuItem.Name = "перезапуститьПроцессToolStripMenuItem";
+            this.перезапуститьПроцессToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.перезапуститьПроцессToolStripMenuItem.Text = "перезапустить процесс";
+            this.перезапуститьПроцессToolStripMenuItem.Click += new System.EventHandler(this.перезапуститьПроцессToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(391, 354);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = ":::";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 412);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "lite IP scener";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem действиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem перезапуститьПроцессToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem пускстопToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ Lokal/lokal/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Threading;
+namespace lokal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+        }
+        int t = 1, t1 = 0;
+        Ping ping = new Ping();
+        Thread th;
+        string[,] mass = new string[254, 3];
+        PingReply r;
+        int c1 = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = 254;
+            progressBar1.Visible = false;
+          
+        }
+        private void pinger()
+        {
+            int t2 = 0;
+            int c = 0;
+            while (true)
+            {
+                string ip = $"192.168.0.{t2}";
+                r = ping.Send(ip, 500);
+                if (r.Status == IPStatus.Success)
+                {
+                    try
+                    {
+                        IPHostEntry host = Dns.GetHostEntry(ip);
+                        //label2.Invoke(new Action(() => { label2.Text = $"Найдено : {t1}"; }));
+                        mass[c, 0] = host.HostName;
+                        mass[c, 1] = r.Address.ToString();
+                        mass[c, 2] = r.RoundtripTime.ToString();
+                        if (c == 254) break;
+                        else
+                        {
+                            c++;
+                            c1 = c;
+                        }
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+                t2++;
+                t1 = t2;
+            }    
+        }    
+        private void timer1_Tick(object sender, EventArgs e)
+        {          
+            if (t == 254)
+            {
+                timer1.Enabled = false;
+                label1.Text = $"Готово";
+            }
+            for (int i = 0; i < 254; i++)
+            {
+                    dataGridView1.Rows[i].Cells[0].Value = mass[i, 0];
+                    dataGridView1.Rows[i].Cells[1].Value = mass[i, 1];
+                    dataGridView1.Rows[i].Cells[2].Value = mass[i, 2];
+            }    
+            label2.Text = $"Найдено : {c1}";
+            label1.Text =  $"пройдено {t1} из 254";
+            progressBar1.Value = t;          
+            t++;
+        }
+        private void пускстопToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            th = new Thread(pinger);
+            th.Start();
+            dataGridView1.RowCount = 254;
+            progressBar1.Visible = true;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (th != null)  th.Abort(); 
+        }
+
+        private void перезапуститьПроцессToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            t = 1;
+            t1 = 0;
+            for (int i = 0; i < dataGridView1.RowCount; i++) for (int j = 0; j < 3; j++)  dataGridView1.Rows[i].Cells[j].Value = "";
+        }
+    }
+}
